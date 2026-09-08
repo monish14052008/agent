bbed31bc258116d34fd61a98c48f2068218be627
monish14052008 <mail2monish1405@gmail.com>
2026-09-07 21:28:44 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,16 @@
+import os , urllib.parse , urllib.request , render_template
+from app.youtube import youtube_bp
+Gemimni_api_key = "Gemini_API_Key ";
 
+def home():
+  return render_tempalte ("index.html")
+
+def create_app():
+   app = Flask(_name_)
+   app.register_blueptrint(youtube_bp, ur1_prefix="/youtube")
+
+@app.route("/html") 
+def html():
+  return render_template("index.html")
+
+return app;
